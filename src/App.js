35972380bc6d2e836e21469490e4d36bc17da990
Nontapat Pintira <nontapat.pin@gmail.com>
2024-01-10 21:29:39 +0700

    feat: revamp

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
   const [enhancementTwo, setEnhancementTwo] = useState(0);
   const [enhancementThree, setEnhancementThree] = useState(0);
   const [enhancementFour, setEnhancementFour] = useState(0);
-  const [fragment, setFragment] = useState(0);
-  const [dailyIncome, setDailyIncome] = useState(12);
+  const [income, setIncome] = useState(12);
 
   const getOriginCost = (level) => {
     const costs = [
@@ -35,6 +34,22 @@ function App() {
     return costs.slice(0, level).reduce((a, b) => a + b, 0);
   };
 
+  const getFragmentSpent = () => {
+    return (
+      getOriginCost(origin) +
+      getMasteryCost(mastery) +
+      getEnhancementCost(enhancementOne) +
+      getEnhancementCost(enhancementTwo) +
+      getEnhancementCost(enhancementThree) +
+      getEnhancementCost(enhancementFour)
+    );
+  };
+
+  const TOTAL_ORIGIN = getOriginCost(30);
+  const TOTAL_MASTERY = getMasteryCost(30);
+  const TOTAL_ENHANCEMENT = getEnhancementCost(30);
+  const TOTAL_FRAGS = TOTAL_ORIGIN + TOTAL_MASTERY + 4 * TOTAL_ENHANCEMENT;
+
   useEffect(() => {
     document.title = "Hoop's Erda Fragments Calculator";
   }, []);
@@ -42,86 +57,136 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <div className='vertical'>
-          <h1>Erda Fragments Calculator</h1>
-          <img className='center' src='7308956572081502428.png' />
-          <p>Input your current HEXA Skill level</p>
-          <div className='horizontal'>
-            <span>Origin Core Level </span>
+        <h1>Erda Fragments Calculator</h1>
+        <img className='center' src='7308956572081502428.png' />
+        <p>Input your current HEXA Skill level</p>
+        <section>
+          <header>
+            <div className='col'>Skill</div>
+            <div className='col'>Current Level</div>
+            <div className='col'>Fragments Spent</div>
+            <div className='col'>Fragments Left</div>
+            <div className='col'>Total</div>
+          </header>
+          <div className='row'>
+            <div className='col'>Origin</div>
             <input
               id='origin-level'
               type='number'
+              value={origin}
               onChange={(e) => setOrigin(e.target.value)}></input>
+            <div className='col'>{getOriginCost(origin)}</div>
+            <div className='col'>{TOTAL_ORIGIN - getOriginCost(origin)}</div>
+            <div className='col'>{TOTAL_ORIGIN}</div>
           </div>
-          <div className='horizontal'>
-            <span>Mastery Core Level </span>
+          <div className='row'>
+            <div className='col'>Mastery</div>
             <input
               id='mastery-level'
               type='number'
+              value={mastery}
               onChange={(e) => setMastery(e.target.value)}></input>
+            <div className='col'>{getMasteryCost(mastery)}</div>
+            <div className='col'>{TOTAL_MASTERY - getMasteryCost(mastery)}</div>
+            <div className='col'>{TOTAL_MASTERY}</div>
           </div>
-          <div className='horizontal'>
-            <span>Enhancement Core #1 Level </span>
+          <div className='row'>
+            <div className='col'>Enhancement#1</div>
             <input
               id='enhancement-one-level'
               type='number'
+              value={enhancementOne}
               onChange={(e) => setEnhancementOne(e.target.value)}></input>
+            <div className='col'>{getEnhancementCost(enhancementOne)}</div>
+            <div className='col'>
+              {TOTAL_ENHANCEMENT - getEnhancementCost(enhancementOne)}
+            </div>
+            <div className='col'>{TOTAL_ENHANCEMENT}</div>
           </div>
-          <div className='horizontal'>
-            <span>Enhancement Core #2 Level </span>
+          <div className='row'>
+            <div className='col'>Enhancement#2</div>
             <input
               id='enhancement-two-level'
               type='number'
+              value={enhancementTwo}
               onChange={(e) => setEnhancementTwo(e.target.value)}></input>
+            <div className='col'>{getEnhancementCost(enhancementTwo)}</div>
+            <div className='col'>
+              {TOTAL_ENHANCEMENT - getEnhancementCost(enhancementTwo)}
+            </div>
+            <div className='col'>{TOTAL_ENHANCEMENT}</div>
           </div>
-          <div className='horizontal'>
-            <span>Enhancement Core #3 Level </span>
+          <div className='row'>
+            <div className='col'>Enhancement#3</div>
             <input
               id='enhancement-three-level'
               type='number'
+              value={enhancementThree}
               onChange={(e) => setEnhancementThree(e.target.value)}></input>
+            <div className='col'>{getEnhancementCost(enhancementThree)}</div>
+            <div className='col'>
+              {TOTAL_ENHANCEMENT - getEnhancementCost(enhancementThree)}
+            </div>
+            <div className='col'>{TOTAL_ENHANCEMENT}</div>
           </div>
-          <div className='horizontal'>
-            <span>Enhancement Core #4 Level </span>
+          <div className='row'>
+            <div className='col'>Enhancement#4</div>
             <input
               id='enhancement-four-level'
               type='number'
+              value={enhancementFour}
               onChange={(e) => setEnhancementFour(e.target.value)}></input>
+            <div className='col'>{getEnhancementCost(enhancementFour)}</div>
+            <div className='col'>
+              {TOTAL_ENHANCEMENT - getEnhancementCost(enhancementFour)}
+            </div>
+            <div className='col'>{TOTAL_ENHANCEMENT}</div>
           </div>
-          <div className='horizontal'>
-            <span>Daily Fragment Income (Not Including Daily) </span>
+          <div className='row'>
+            <div className='col'>Total</div>
+            <div className='col'></div>
+            <div className='col'>{getFragmentSpent()}</div>
+            <div className='col'>{TOTAL_FRAGS - getFragmentSpent()}</div>
+            <div className='col'>{TOTAL_FRAGS}</div>
+          </div>
+          <div className='row'>
+            <div className='col'>Completion</div>
+            <div className='col'>
+              {parseFloat((getFragmentSpent() / TOTAL_FRAGS) * 100).toFixed(2)}%
+            </div>
+            <div className='col'></div>
+            <div className='col'></div>
+            <div className='col'></div>
+          </div>
+          <div className='row'>
+            <div className='col'></div>
+            <div className='col'></div>
+            <div className='col'>Frags/hr</div>
+            <div className='col'>Time Left (hr)</div>
+            <div className='col'>Total Time</div>
+          </div>
+          <div className='row'>
+            <div className='col'></div>
+            <div className='col'></div>
             <input
-              id='daily-income'
+              id='frags-per-hour'
               type='number'
-              onChange={(e) => setDailyIncome(e.target.value)}></input>
+              value={income}
+              onChange={(e) => setIncome(e.target.value)}></input>
+            <div className='col'>
+              {income <= 0
+                ? "N/A"
+                : parseFloat(
+                    (TOTAL_FRAGS - getFragmentSpent()) / income
+                  ).toFixed(1)}
+            </div>
+            <div className='col'>
+              {income <= 0
+                ? "N/A"
+                : parseFloat(TOTAL_FRAGS / income).toFixed(1)}
+            </div>
           </div>
-          <button
-            style={{
-              background: "#ffa31a",
-              color: "#ffffff",
-              fontSize: "24px",
-            }}
-            onClick={(e) => {
-              const result =
-                getOriginCost(origin) +
-                getMasteryCost(mastery) +
-                getEnhancementCost(enhancementOne) +
-                getEnhancementCost(enhancementTwo) +
-                getEnhancementCost(enhancementThree) +
-                getEnhancementCost(enhancementFour);
-              setFragment(result);
-            }}>
-            Calculate
-          </button>
-        </div>
-        <span id='results' style={{ padding: "10px" }}>
-          You've spent {fragment} fragments already. You need {20184 - fragment}{" "}
-          more fragments.
-        </span>
-        <span id='results' style={{ padding: "10px" }}>
-          With your current fragment income, it will take you{" "}
-          {(20184 - fragment) / dailyIncome} days to max HEXA skill.
-        </span>
+        </section>
       </header>
     </div>
   );
